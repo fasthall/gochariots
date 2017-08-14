823eed16b6b69297cec579ea26a37da70afb3c73
Wei-Tsung Lin <fasthall@gmail.com>
2017-08-14 13:56:23 -0700

    don't connect to cosmos db until environment var set

--- maintainer/adapter/cosmos/cosmos.go
+++ maintainer/adapter/cosmos/cosmos.go
@@ -23,6 +23,10 @@ func init() {
 	DATABASE = os.Getenv("COSMOS_USERNAME")
 	PASSWORD = os.Getenv("COSMOS_PASSWORD")
 
+	if DATABASE == "" || PASSWORD == "" {
+		return
+	}
+
 	// DialInfo holds options for establishing a session with a MongoDB cluster.
 	dialInfo := &mgo.DialInfo{
 		Addrs:    []string{fmt.Sprintf("%s.documents.azure.com:10255", DATABASE)}, // Get HOST + PORT
