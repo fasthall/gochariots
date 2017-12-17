366d749354bca5ced9dd6f028992e1d4e575ced3
Wei-Tsung Lin <fasthall@gmail.com>
2017-12-17 17:04:28 +0800

    benchmark logs throughput

--- misc/benchmark.go
+++ misc/benchmark.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Benchmark struct {
+	start           time.Time
 	lastCount       int
 	recordCount     int
 	loggingAccuracy int
@@ -18,15 +19,20 @@ func NewBenchmark(accuracy int) Benchmark {
 	b := Benchmark{
 		lastCount:       0,
 		loggingAccuracy: accuracy,
+		start:           time.Time{},
 	}
 	return b
 }
 
 func (b *Benchmark) Logging(incoming int) {
 	b.mutex.Lock()
+	if b.start.Equal(time.Time{}) {
+		b.start = time.Now()
+	}
 	b.recordCount += incoming
 	if b.loggingAccuracy > 0 && b.recordCount-b.lastCount >= b.loggingAccuracy {
-		logrus.WithFields(logrus.Fields{"records": b.recordCount, "timestamp": time.Now()}).Info("benchmark logging")
+		throughput := float64(b.recordCount) / time.Since(b.start).Seconds()
+		logrus.WithFields(logrus.Fields{"throughput": throughput, "records": b.recordCount, "timestamp": time.Now()}).Info("benchmark logging")
 		b.lastCount = b.recordCount
 	}
 	b.mutex.Unlock()
